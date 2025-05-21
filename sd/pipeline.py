373f63837fdb86388dc2aecd4ce36cb156f79520
Eric Du <ericdu117@gmail.com>
2025-05-21 11:58:40 -0700

    Completed pipeline

--- sd/pipeline.py
+++ sd/pipeline.py
@@ -10,7 +10,7 @@ LATENTS_HEIGHT = HEIGHT // 8
 
 def generate(
     prompt: str,
-    uncond_prompt: str,
+    uncond_prompt: str, #negative prompt or empty string
     input_image=None,
     strength=0.8,
     do_cfg=True,
@@ -44,4 +44,121 @@ def generate(
         clip = models["clip"]
         clip = clip.to(device)
 
+        if do_cfg:
+            #Convert prompt into tokens using tokenizer
+            cond_tokens = tokenizer.batch_encode_plus([prompt], padding="max_length", max_length=77).input_ids
+            # (BS, Seq_Len)
+            cond_tokens = torch.tensor(cond_tokens, dtpye=torch.long, device=device)
+            # (BS, Seq_Len) -> (BS, Seq_Len, Dim)
+            cond_context = clip(cond_tokens)
 
+            uncond_tokens = tokenizer.batch_encode_plus([uncond_prompt], padding="max_length", max_length=77).input_ids
+            uncond_tokens = torch.tensor(uncond_tokens, dtype=torch.long, device=device)
+            # (BS, Seq_Len) -> (BS, Seq_Len, Dim)
+            uncond_context = clip(uncond_tokens)
+
+            # (2, Seq_Len, Dim) = (2, 77, 768)
+            context = torch.cat(context, uncond_context)
+        else:
+            #Convert it into a list of tokens
+            tokens = tokenizer.batch_encode_plus([prompt], padding="max_length", max_length=77).input_ids
+            tokens = torch.tensor(tokens, dtype=torch.long, device=device)
+            # (1, 77, 768)
+            context = clip(tokens)
+        to_idle(clip)
+
+        #detects sampler
+        if sampler_name == "ddpm":
+            sampler = DDPMSampler(generator)
+            sampler.set_inference_steps(n_inference_steps)
+        else:
+            raise ValueError(f"Unknown {sampler_name}")
+        
+        latents_shape = (1, 4, LATENTS_HEIGHT, LATENTS_WIDTH)
+
+        if input_image:
+            encoder = models["encoder"]
+            encoder.to(device)
+
+            input_image_tensor = input_image.resize((WIDTH, HEIGHT))
+            input_image_tensor = np.array(input_image_tensor)
+            # (Height, Width, Chart)
+            input_image_tensor = torch.tensor(input_image_tensor, dtype=torch.float32)
+            input_image_tensor = rescale(input_image_tensor, (0, 255), (-1, 1))
+            # (Height, Width, Channel) -> (Batch_Size, Height, Width, Channel)
+            input_image_tensor = input_image_tensor.unsqueeze(0)
+            # (Batch_Size, Height, Width, Channel) -> (BS, Channel, Height, Width)
+            input_image_tensor = input_image_tensor.permute(0, 3, 1, 2)
+
+            encoder_noise = torch.rand(latents_shape, generator=generator, device=device)
+            #run image through encoder of VAE
+            latents = encoder(input_image_tensor, encoder_noise)
+
+            #add noise to latents
+            sampler.set_strength(strength=strength)
+            latents = sampler.add_noise(latents, sampler.timesteps[0])
+
+            to_idle(encoder)
+        else:
+            #if we are doing text-to-image, start with random noise N(0, I)
+            latents = torch.randn(latents_shape, generator=generator, device=device)
+
+        #1000 980 960 940...
+        #For 50 time steps
+            
+        diffusion = models["diffusion"]
+        diffusion.to(device)
+
+        timesteps = tqdm(sampler.timesteps)
+        for i, timestep in enumerate(timesteps):
+            # (1, 320)
+            time_embedding = get_time_embedding(timestep).to(device)
+
+            # (BS, 4, Latents_Height, Latents_Width)
+            model_input = latents
+
+            if do_cfg:
+                #(BS, 4, Latents_Height, Latents_Width) -> (2 * Batch_Size, 4, Latents_Height, Latents_Width)
+                model_input = model_input.repeat(2, 1, 1, 1)
+            
+            #model_output is predicted noise by UNET
+            model_output = diffusion(model_input, context, time_embedding)
+
+            if do_cfg:
+                output_cond, output_uncond = model_output.chunk(2)
+                model_output = cfg_scale * (output_cond - output_uncond) + output_uncond
+
+            #remove noise preditcted by UNET
+            latents = sampler.step(timestep, latents, model_output)
+
+        to_idle(diffusion)
+
+        decoder = models["decoder"]
+        decoder.to(device)
+
+        images = decoder(latents)
+        to_idle(decoder)
+
+        images = rescale(images, (-1, 1), (0, 255), clamp=True)
+        # (BS, Channel, Height, Width) -> (BS, Height, Width, Channel)
+        images = images.permute(0, 2, 3, 1)
+        images = images.to("cpu", torch.uint8).numpy()
+        return images[0]
+    
+def rescale(x, old_range, new_range, clamp=False):
+    old_min, old_max = old_range
+    new_min, new_max = new_range
+    x -= old_min
+    x *= (new_max - new_min) / (old_max - old_min)
+    x += new_min
+    if clamp:
+        x = x.clamp(new_min, new_max)
+    return x
+
+def get_time_embedding(timestep):
+    #(160, )
+    freqs = torch.pow(10000, -torch.arange(start = 0, end=160, type=torch.float32) / 160)
+    #(1, 160)
+    x = torch.tensor([timestep], dtype=torch.float32)[:, None] * freqs[None]
+    #(1, 320)
+    return torch.cat([torch.cos(x), torch.sin(x)], dim=-1)
